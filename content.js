ed400811a066aa1eedb4f47aaa2387c01e431581
Agne <agne@lukoseviciute.com>
2019-09-05 16:47:49 +0200

    added message passing

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 //alert("Hello from your Chrome extension!")
 
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-console.log(firstHref);
+      console.log(firstHref);
+    }
+  }
+);
